f417ea9821dc32c9cb293b844433b1b59e0b30a0
michaelyaugogox <michael.yau@gogox.com>
2026-01-09 11:00:38 +0800

    first commit

---
+++ libs/logger.js
@@ -0,0 +1,27 @@
+const ActivityLog = require("../models/ActivityLogmodel");
+
+const logActivity = async ({
+  action,
+  description,
+  entity,
+  entityId,
+  userId,
+  ipAddress,
+}) => {
+  try {
+    const newActivity = new ActivityLog({
+      action,
+      description,
+      entity,
+      entityId,
+      userId,
+      ipAddress,
+    });
+
+    await newActivity.save();
+  } catch (error) {
+    console.error("Error logging activity:", error);
+  }
+};
+
+module.exports = logActivity;
